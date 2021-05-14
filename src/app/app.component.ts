e729d25888a9a140281ff3cb634f6f19236eea86
manjeetdhimaan <manjeetdhimaan80@gmail.com>
2021-05-14 10:16:23 +0530

    updated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+loadedFeature:string = 'home' 
+
+  onNavigate(features:string){
+ this.loadedFeature = features;
+  }
 }
